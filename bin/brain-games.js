ecdb2dc9ccdf749972403d10fa0d4ae7069b69b7
Alexey <alexey.khusnullin2015@yandex.ru>
2025-03-21 01:34:41 +0400

    update brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { greetingUser } from '../src/cli.js'
+import cli from '../src/cli.js'
 
-console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(greetingUser(name));
+cli();
+
+
