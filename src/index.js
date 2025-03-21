9215b8d0ddea8c05dc1523ab482aab9382f7959b
Alexey <alexey.khusnullin2015@yandex.ru>
2025-03-21 07:54:05 +0400

    add src/index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import readlineSync from 'readline-sync';
+
+export const pattern = (descriptionTask, getExpressionAndAnswer) => {
+	console.log('Welcome to the Brain Games!');
+	const name = readlineSync.question('May I have your name? ');
+	console.log(`Hello, ${name}`);
+	console.log(descriptionTask);
+	for (let i = 0; i < 3; i += 1) {
+		const [expression, currentAnswer] = getExpressionAndAnswer();
+		console.log(`Question: ${expression}`);
+		const userAnswer = readlineSync.question('Your answer: ');
+		if (userAnswer === currentAnswer) {
+			console.log('Correct!');
+		} else {
+			return console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${currentAnswer}'.\nLet's try again, ${name}!`);
+		}
+	}
+	return console.log(`Congratulations, ${name}`);
+
+}
